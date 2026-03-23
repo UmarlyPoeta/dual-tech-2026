090625d429eab913f6cd0856e9680c423efb94fd
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2026-03-23 16:30:46 +0100

    Add picamera2 to requirements and create setup script for pyenv installation

---
+++ fix_shit.sh
@@ -0,0 +1,26 @@
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+exec "$SHELL"
+
+sudo apt install -y \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libffi-dev \
+  liblzma-dev \
+  libncursesw5-dev
+
+pyenv install 3.11.8
+
+pyenv global 3.11.8
+
+
+sudo apt update
+sudo apt install libcap-dev
